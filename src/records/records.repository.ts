c1820818954026a053402db206faa9a3064d0063
Nastassia Shkutovich <yatsan.shmat@gmail.com>
2025-02-03 16:18:08 +0300

    #3: defined records repository layer.

---
+++ src/records/records.repository.ts
@@ -0,0 +1,33 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+
+import { Repository } from 'typeorm';
+
+import { Record } from './entities/record.entity';
+import { CreateRecordDto } from './dto/create-record.dto';
+import { UpdateRecordDto } from './dto/update-record.dto';
+
+@Injectable()
+export class RecordRepository {
+  constructor(@InjectRepository(Record) private recordsRepository: Repository<Record>) {}
+
+  create(createRecordDto: CreateRecordDto) {
+    return this.recordsRepository.save(createRecordDto);
+  }
+
+  findAll(): Promise<Record[]> {
+    return this.recordsRepository.find();
+  }
+
+  findOne(id: number): Promise<Record | null> {
+    return this.recordsRepository.findOneBy({ id });
+  }
+
+  async updateOne(id: number, updateUserDto: UpdateRecordDto): Promise<void> {
+    await this.recordsRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.recordsRepository.delete(id);
+  }
+}
