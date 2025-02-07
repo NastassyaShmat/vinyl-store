79376a845e0d890725c5d7e68a425d09f07a4033
Nastassia Shkutovich <yatsan.shmat@gmail.com>
2025-02-07 14:29:48 +0300

    #5: cookie setting.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(passport.initialize());
+  app.use(cookieParser(configService.get<string>('JWT_SECRET')));
 
   const config = new DocumentBuilder()
     .setTitle('Vinyl Store')
